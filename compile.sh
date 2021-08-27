22f5efa24eff5dac5f7cc922391a70c5ab0e816a
shownb <micoach@19cn.com>
2021-08-27 09:38:15 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
- version="21.02.0-rc4"
- wget -q https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
- xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
- tar -xf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
- cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
- ls -la
- wget -P ./target/linux/ramips/image https://github.com/Carrjiang/Actions-OpenWrt_PHICOMM_K2/raw/master/mt7620.mk -O mt7620.mk
- wget -P ./target/linux/ramips/dts https://github.com/Carrjiang/Actions-OpenWrt_PHICOMM_K2/raw/master/mt7620a_phicomm_psg1218.dtsi -O mt7620a_phicomm_psg1218.dtsi
- #make info
- make image PROFILE=phicomm_psg1218a PACKAGES="uhttpd uhttpd-mod-ubus luci-theme-bootstrap luci-app-opkg luci-base luci-app-firewall luci-mod-admin-full -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only"
+version="21.02.0-rc4"
+wget -q https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
+xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
+tar -xf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
+cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
+ls -la
+wget -P ./target/linux/ramips/image https://github.com/shownb/k2_openwrt_16m/raw/master/mt7620.mk -O mt7620.mk
+wget -P ./target/linux/ramips/dts https://github.com/shownb/k2_openwrt_16m/raw/master/mt7620a_phicomm_psg1218.dtsi -O mt7620a_phicomm_psg1218.dtsi
+#make info
+make image PROFILE=phicomm_psg1218a PACKAGES="uhttpd uhttpd-mod-ubus luci-theme-bootstrap luci-app-opkg luci-base luci-app-firewall luci-mod-admin-full -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only"
 #uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-app-ddns -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only
 #uhttpd luci-base luci-app-shadowsocks-libev shadowsocks-libev-ss-local -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only
