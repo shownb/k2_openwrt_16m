b0beddf6fef70cf746014fa5e61050a22b53ace3
shownb <micoach@19cn.com>
2025-01-11 15:56:15 -0500

    Update compile.sh

--- 23.05.5/compile.sh
+++ 23.05.5/compile.sh
@@ -7,5 +7,4 @@ tar -xf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
 curl https://raw.githubusercontent.com/shownb/k2_openwrt_16m/main/23.05.5/mt7620.mk -L -o ./target/linux/ramips/image/mt7620.mk
 curl https://raw.githubusercontent.com/shownb/k2_openwrt_16m/main/23.05.5/mt7620a_phicomm_k2-v22.4.dts -L -o ./target/linux/ramips/dts/mt7620a_phicomm_k2-v22.4.dts
-make info
 make image PROFILE=phicomm_k2-v22.4 PACKAGES="uhttpd uhttpd-mod-ubus luci-theme-bootstrap luci-app-opkg luci-base luci-app-firewall luci-mod-admin-full -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only luci-proto-wireguard kmod-wireguard"
