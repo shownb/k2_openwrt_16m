e6fa32cfb77dc20399af6dbc8a3a77e10d8c6056
shownb <micoach@19cn.com>
2025-01-11 13:31:27 -0500

    Update compile.sh
    去处烦人的信息

--- 23.05.5/compile.sh
+++ 23.05.5/compile.sh
@@ -1,9 +1,9 @@
  #good for you
 version="23.05.5"
 current_path = $(pwd)
-wget https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
-xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
-tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
+wget -q https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
+xz -dq openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
+tar -xf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
 curl https://raw.githubusercontent.com/shownb/k2_openwrt_16m/main/23.05.5/mt7620.mk -L -o ./target/linux/ramips/image/mt7620.mk
 curl https://raw.githubusercontent.com/shownb/k2_openwrt_16m/main/23.05.5/mt7620a_phicomm_k2-v22.4.dts -L -o ./target/linux/ramips/dts/mt7620a_phicomm_k2-v22.4.dts
